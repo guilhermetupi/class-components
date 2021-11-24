59666371e0233697e4662165afb85e676e38ae83
Guilherme Tupinambá Durães <40327476+guilhermetupi@users.noreply.github.com>
2021-11-24 01:05:51 -0300

    monitorar mudancas classe

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -3,21 +3,33 @@ import React, { createContext, useState } from 'react';
 export const ThemeContext = createContext();
 
 export class ThemeProvider extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    let theme = 'dark';
+
+    try {
+      theme = JSON.parse(localStorage.getItem('theme'));
+    } catch (e) {
+      console.error(e);
+    }
+
+    this.state = { theme };
   }
 
-  state = {
-    theme: 'dark',
+  componentDidUpdate(_, prevState) {
+    if (prevState.theme !== this.state.theme) { // monitorar mudanças no estado
+      console.log('tema mudou')
+    }
   }
 
   handleToggleTheme = () => {
-    this.setState((prevState) => ({ theme: prevState.theme === 'dark' ? 'light' : 'dark' }));
+    this.setState((prevState) => ({ theme: prevState.theme === 'dark' ? 'light' : 'dark' }), () => localStorage.setItem('theme', JSON.stringify(this.state.theme)));
   }
 
   render() {
     return (
-      <ThemeContext.Provider value={{ theme: this.state.theme, handleToggleTheme: this.handleToggleTheme }}>
+      <ThemeContext.Provider value={{ theme: this.state.theme || 'dark', handleToggleTheme: this.handleToggleTheme }}>
         {this.props.children}
       </ThemeContext.Provider>
     );
